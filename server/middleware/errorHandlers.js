e1138052443dc6c1994f2058cd0e2782280acf6b
zakariahn <zakaria@posteo.net>
2025-01-21 12:32:55 +0100

    adding http-errors dpcy

--- server/middleware/errorHandlers.js
+++ server/middleware/errorHandlers.js
@@ -1,27 +1,5 @@
 import createError from "http-errors";
 
-export const duplicateFieldsHandler = (keyValue) => {
-  const field = Object.keys(keyValue)[0];
-  return createError(`${field} is already exist`);
-};
-
-export const isValidId = (req) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id))
-    throw createError("The provided ID is invalid.");
-};
-
-export const resourceNotFound = (resource, document, action) => {
-  if (!resource)
-    throw createError(
-      404,
-      `Sorry, the ${document} you are trying to ${action} doesn't seem to exist.`
-    );
-};
-
-export const authError = (statusCode, message) => {
-  throw createError(statusCode, message);
-};
-
 export const routeNotFound = () => {
   throw createError(404, "Page was not found");
 };
